40daf359b9dce07600e83a465b0c815f68d9edbb
Ramon Domingos <ramongemio@gmail.com>
2024-04-22 22:18:53 -0300

    add cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,11 +2,13 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import {router} from "./routes/products.routes";
 import { db} from './db.config';
+import cors from 'cors';
+
 const REDIS_URL = process.env.REDIS_URL || 'localhost'
 const REDIS_PORT = parseInt(process.env.REDIS_PORT || '6379')
 const REDIS_EXPIRE = parseInt(process.env.REDIS_EXPIRE || `10`);
 dotenv.config();
-
+var allowedOrigins = ['*'];
 let cache = require('express-redis-cache')({
     host: REDIS_URL, port: REDIS_PORT, expire: REDIS_EXPIRE
 });
@@ -14,7 +16,13 @@ let cache = require('express-redis-cache')({
 
 const app: Express = express();
 const port = process.env.PORT ;
-
+app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
